4b08a627874e0aa14e9f00bdd265f914e90a000a
Hjalmar Meza <hjalmar.meza@gmail.com>
2026-04-28 22:05:56 +0200

    🚨 Cache Reset: Killing Service Worker to force update.

--- sw.js
+++ sw.js
@@ -1,39 +1,11 @@
-const CACHE_NAME = 'musichris-themes-v4';
-const ASSETS = [
-  './',
-  'index.html',
-  'version.json',
-  'assets/logo_app.png',
-  'assets/master_template.png'
-];
-
-self.addEventListener('install', (event) => {
+self.addEventListener('install', (e) => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    }).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('activate', (e) => {
+  self.registration.unregister()
+    .then(() => self.clients.matchAll())
+    .then((clients) => {
+      clients.forEach(client => client.navigate(client.url));
+    });
 });
