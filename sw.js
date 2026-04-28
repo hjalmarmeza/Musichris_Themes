17bac0e0c3d721d5560fcd8c314875a3da126f12
Hjalmar Meza <hjalmar.meza@gmail.com>
2026-04-28 21:34:22 +0200

    🔄 PWA Update: Bumped to v3 to force cache refresh and apply gear relocation fix.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'musichris-themes-v2';
+const CACHE_NAME = 'musichris-themes-v3';
 const ASSETS = [
   './',
   'index.html',
@@ -7,9 +7,24 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
